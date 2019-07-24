dcbc812ac743373cf698ab80e1c807a17fc55ef0
gabiflorea10 <floreagabi_fga@yahoo.com>
2019-07-24 16:12:34 +0300

    TVB-2493 Clean up cfflib files. Add copyright to cfflib files.

--- tvb/adapters/uploaders/cff/loadsave.py
+++ tvb/adapters/uploaders/cff/loadsave.py
@@ -1,4 +1,64 @@
-""" Routines to load, save a connectome files """
+# -*- coding: utf-8 -*-
+#
+#
+# Copyright (C) 2009-2011, Ecole Polytechnique Fédérale de Lausanne (EPFL) and
+# Hospital Center and University of Lausanne (UNIL-CHUV), Switzerland
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Ecole Polytechnique Fédérale de Lausanne (EPFL)
+#       and Hospital Center and University of Lausanne (UNIL-CHUV) nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL "Ecole Polytechnique Fédérale de Lausanne (EPFL) and
+# Hospital Center and University of Lausanne (UNIL-CHUV)" BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Library adapted by The Virtual Brain team - July 2019
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
 
 #import cfflib_modified as cfflib
 #import cfflib
@@ -166,11 +226,6 @@ def save_to_cff(connectome, filename):
             print("Element has no .src attribute. Create it and set it to %s" % ele.src)
         
         if not hasattr(ele, 'data'):
-            
-            # Add if iszip is undefined
-#            if not hasattr(connectome, 'iszip'):
-#                connectome.iszip = False
-            
             if connectome.iszip:
                 # extract zip content and add it to new zipfile
                 if not wt in connectome._zipfile.namelist():
@@ -181,10 +236,6 @@ def save_to_cff(connectome, filename):
                 else:
                     ftmp = connectome._zipfile.extract(wt)
             else:
-                # connectome was not created from a zip
-                # but for example in an IPython session
-                
-                # if now fname
                 if not hasattr(connectome, 'fname'):
                     connectome.fname = op.abspath(filename)
                 
@@ -201,12 +252,6 @@ def save_to_cff(connectome, filename):
                     except:
                         # e.g. there is nothing to save exception
                         ftmp = op.join(op.dirname(connectome.fname), wt)
-
-                # use the saved location
-#                ftmp = ele.tmpsrc
-                
-                # create path coming from filesystem
-                #ftmp = op.join(op.dirname(connectome.fname), wt)
                 
                 if not op.exists(ftmp):
                     msg = """There exists no file %s for element %s. 
